07661fba2601f87a1e660e9f04bf0691eb1451ee
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2019-06-04 16:58:10 -0500

    add view to device

--- app/src/main/java/mx/com/nut/testcleverness/presentation/activity/MainActivity.kt
+++ app/src/main/java/mx/com/nut/testcleverness/presentation/activity/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity(),DevicesAdapter.OnItemClickListener {
         setContentView(R.layout.activity_main)
         serviceHub = Service()
         adapter = DevicesAdapter(this,this)
-        rvDevices.layoutManager = GridLayoutManager(this,3)
+        rvDevices.layoutManager = GridLayoutManager(this,2)
 
         rvDevices.adapter = adapter
 
